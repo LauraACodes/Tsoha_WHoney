f87bf8595d98b8796f0d09ca87d8db42fbb8d6ae
Andström Laura <andlaura@cubbli20-gold.local>
2020-10-02 14:47:53 +0300

    Enrage with db.py

--- db.py
+++ db.py
@@ -3,3 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 # define db address, disable tracking (takes space) 
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app);
